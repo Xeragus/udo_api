1549991619cc745afe3ad888635d1ec67dc72e2b
Boban Sugareski <boban.s@slicelife.com>
2020-09-15 20:32:52 +0200

    Add progress adding logic

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -28,7 +28,15 @@ class GoalsController < ApplicationController
 
   # PATCH/PUT /goals/1
   def update
-    if @goal.update(goal_params)
+    update_data = goal_params
+
+    if params[:add_progress].present?
+      update_data[:current_progress] = @goal.current_progress + params[:add_progress]
+    end
+
+    if @goal.update(update_data)
+      @goal.update({ status: 'completed' }) if @goal.current_progress >= @goal.target
+
       render json: @goal
     else
       render json: @goal.errors, status: :unprocessable_entity
